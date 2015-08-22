b6a4572596e1ec1432c1c5e65b5cb90f73115b34
Wlad Dicario <wlad@2v-prod.com>
2015-08-22 04:02:40 +0200

    Initial Commit

---
+++ Protocol-Oriented Swift 2UITests/Protocol_Oriented_Swift_2UITests.swift
@@ -0,0 +1,34 @@
+//
+//  Protocol_Oriented_Swift_2UITests.swift
+//  Protocol-Oriented Swift 2UITests
+//
+//  Created by Wlad Dicario on 22/08/2015.
+//  Copyright © 2015 Sweefties. All rights reserved.
+//
+
+import XCTest
+
+class Protocol_Oriented_Swift_2UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
