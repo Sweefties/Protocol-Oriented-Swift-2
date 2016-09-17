05cd669c55d5d133f53f715653748b5eec030580
Wladislas Blin <wlad.djclubing@orange.fr>
2016-09-17 09:36:00 +0200

    Update for Xcode 8 - Swift 3

--- Protocol-Oriented Swift 2Tests/Protocol_Oriented_Swift_2Tests.swift
+++ Protocol-Oriented Swift 2Tests/Protocol_Oriented_Swift_2Tests.swift
@@ -28,7 +28,7 @@ class Protocol_Oriented_Swift_2Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Protocol-Oriented-Swift-2
 MVVM Protocol-Oriented Programming Example in Swift 2.
 
-- Require XCode 7, Swift 2.
+## Update for XCode 8 / Swift 3
+
+- Require XCode 8, Swift 3.
 Tested on iOS 9.0
